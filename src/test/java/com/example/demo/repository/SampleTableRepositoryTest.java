c300a39b615cc9f6b753c0cba7cb0ecd2b6d8f29
MACBOOK_MAKO <>
2021-10-07 07:18:26 +0900

    Repositoryテストクラスの定義

---
+++ src/test/java/com/example/demo/repository/SampleTableRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.example.demo.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SampleTableRepositoryTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
